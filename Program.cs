d30cc13d2131695b1e27961fcde09951b42681e6
Barros <mdbf42@gmail.com>
2022-08-22 22:21:04 -0300

    Fix @ Cors

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ JWTConfiguration.Configure(builder);
 
 builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
 {
-    builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+    builder.WithOrigins("https://*.webforest.eco").AllowAnyMethod().AllowAnyHeader();
 }));
 
 builder.Services.AddControllers();
