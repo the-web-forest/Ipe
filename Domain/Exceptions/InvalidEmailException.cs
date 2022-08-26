39d10cda90542edb07e54df84a6617b72766b742
Barros <mdbf42@gmail.com>
2022-08-26 19:46:06 -0300

    Feature @ Google Login - Code Smells

--- Domain/Exceptions/InvalidEmailException.cs
+++ Domain/Exceptions/InvalidEmailException.cs
@@ -1,6 +1,9 @@
-﻿namespace Ipe.Domain.Errors;
+﻿using System.Runtime.Serialization;
+
+namespace Ipe.Domain.Errors;
 
 public class InvalidEmailException : BaseException
 {
     public InvalidEmailException() : base("008", "Invalid Email") { }
+    protected InvalidEmailException(SerializationInfo info, StreamingContext context) : base(info, context) { }
 }
