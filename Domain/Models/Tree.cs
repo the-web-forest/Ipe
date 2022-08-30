e5c439bde57fcd3141540220d975d7bda69d4279
Hiago Murilo Lima Indalecio <hiagoindalecio@outlook.com>
2022-08-30 19:53:40 -0300

    Removing unecessary code and inproving the trees filter

--- Domain/Models/Tree.cs
+++ Domain/Models/Tree.cs
@@ -16,12 +16,5 @@ namespace Ipe.Domain.Models
         public string Biome { get; set; }
         [BsonElement("deleted")]
         public bool Deleted{ get; set; }
-
-        [BsonElement("lowercaseName")]
-        public string? LowercaseName { get; set; }
-        [BsonElement("lowercaseBiome")]
-        public string? LowercaseBiome { get; set; }
-        [BsonElement("lowercaseDescription")]
-        public string? LowercaseDescription { get; set; }
     }
 }
